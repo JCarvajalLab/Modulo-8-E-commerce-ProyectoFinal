0de5440a6f156635198149c8d5d18b0f66de6b3e
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-07-03 14:13:48 -0400

    feat: Se implementa el modelo Categoria con acceso al administrador Django

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Categoria
 
-# Register your models here.
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nombre","disponible",)
+    list_filter = ("disponible",)
+    search_fields = ("nombre",)
+    ordering = ("nombre",)
